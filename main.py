ffab576d38776bc0c9906c2bd4524d6142b9d7c1
Fredrick George F <fredrick.george.f.25@gmail.com>
2025-07-27 18:07:25 +0530

    changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("Loading packages and models...")
 from ExDoc.headerextractor import headerExtractor
 from ExDoc.headerclassifier import HeaderClassifier
 import pathlib
@@ -8,10 +9,16 @@ pdfs = [ pdf for pdf in inputdir.glob("*.pdf")]
 classifier = HeaderClassifier()
 for pdf in pdfs:
     json_path = outputdir / (pdf.stem + ".json")
+    print("Reading "+str(pdf))
     predictions_df = classifier.predict_headers(pdf)
     predictions_df.rename(columns={'predicted_label':'level'},inplace=True)
     outline = predictions_df[predictions_df['level']!='other'].loc[:,["level","text","page"]].to_dict(orient="records")
-    title = (predictions_df[(predictions_df['page']==0) & ((predictions_df['level']=='H1')|(predictions_df['level']=='H2'))].iloc[0])['text']
+    filtered_df = predictions_df[(predictions_df['page'] == 0) &((predictions_df['level'] == 'H1') | (predictions_df['level'] == 'H2'))]
+    if not filtered_df.empty:
+        title = filtered_df.iloc[0]['text']
+    else:
+        title = "Untitled"
     data={"title":title,"outline":outline}
+    print("Writing "+str(json_path))
     with json_path.open("w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
